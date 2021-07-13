d1092470cd16f0e445200ddf6f346d6a71b8ce7a
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-07-14 03:39:57 +0700

    bug in mockup product

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton} from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+import useStyles from './styles'
 
-
-const Product = () => {
+const Product = ( { product }) => {
+    const classes = useStyles();
     return (
-        <div>
-            
-        </div>
+        <Card className = { classes.root}>
+            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price}
+                    </Typography> 
+                </div>
+                <Typography variant="h2" color="textSecondary">{product.description}</Typography>
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
+        </Card>
     )
 }
 
