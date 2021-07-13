6bfe280419f3e2762d6af922729fd5dd59222249
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-07-14 03:45:23 +0700

    done

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -12,14 +12,14 @@ const Products = () => {
     return (
         <main>
             <Grid container justify="center" spacing={4}>
-                {products.map((product) => {
+                {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                         <Product product={product} />
                     </Grid>
-                })}
+                ))}
             </Grid>
         </main>
     );
-}
+};
 
 export default Products;
